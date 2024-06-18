ef760fc01f43ed0624a2eb2034448a0063cd07d8
Haresh Gayakhe <hgayakhe7@gmail.com>
2024-06-18 22:03:14 +0530

    pass parameters to action methods

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -8,5 +8,17 @@ namespace WebApp.Controllers
         {
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id.HasValue)
+            {
+                return new ContentResult { Content = id.ToString() };
+            }
+            else
+            {
+                return new ContentResult { Content = "null content" };
+            }
+        }
     }
 }
