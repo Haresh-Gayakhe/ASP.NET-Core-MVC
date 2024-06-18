ee687b1467d5a933244d89ce234c2d8cebda35b0
Haresh Gayakhe <hgayakhe7@gmail.com>
2024-06-18 20:48:07 +0530

    handle requests with controllers

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller // Name of controller is Home
+    {
+        public string Index() // Name of action method is Index
+        {
+            return "Hello world from action method";
+        }
+
+        public string Error()
+        {
+            return "I have an error here";
+        }
+    }
+}
